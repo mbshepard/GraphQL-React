af58339695cabf4aa760556a75b0ed5623c996d6
Michael Shepard <michael.shepard@ivanti.com>
2022-11-01 15:03:38 -0700

    working through chapter 6

--- Chapter02/src/server/services/graphql/index.js
+++ Chapter02/src/server/services/graphql/index.js
@@ -4,7 +4,7 @@ import Resolvers from './resolvers';
 import Schema from './schema';
 import authDirective from './auth';
 import JWT from 'jsonwebtoken';
-const { JWT_SECRET } = 'awv4BcIzsRysXkhoSAb8t8lNENgXSqBruVlLwd45kGdYjeJHLap9LUJ1t9DTdw36DvLcWs3qEkPyCY6vOyNljlh2Er952h2gDzYwG82rs1qfTzdVIg89KTaQ4SWI1YGY';
+const { JWT_SECRET } = process.env
 
 export default (utils) => {
   const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective('auth');
